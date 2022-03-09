2a743c35ffae28581cd3a39fe7aff801a1109a64
dom111 <dom111@users.noreply.github.com>
2022-03-09 20:37:20 +0000

    Add `City#units` `AdditionalData`.

--- AdditionalData/units.js
+++ AdditionalData/units.js
@@ -3,9 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.getAdditionalData = void 0;
 const UnitRegistry_1 = require("../UnitRegistry");
 const AdditionalData_1 = require("@civ-clone/core-data-object/AdditionalData");
+const City_1 = require("@civ-clone/core-city/City");
 const Player_1 = require("@civ-clone/core-player/Player");
 const Tile_1 = require("@civ-clone/core-world/Tile");
 const getAdditionalData = (unitRegistry = UnitRegistry_1.instance) => [
+    new AdditionalData_1.default(City_1.default, 'units', (city) => unitRegistry.getByCity(city)),
     new AdditionalData_1.default(Player_1.default, 'units', (player) => unitRegistry.getByPlayer(player)),
     new AdditionalData_1.default(Tile_1.default, 'units', (tile) => unitRegistry.getByTile(tile)),
 ];
--- AdditionalData/units.ts
+++ AdditionalData/units.ts
@@ -3,12 +3,16 @@ import {
   instance as unitRegistryInstance,
 } from '../UnitRegistry';
 import AdditionalData from '@civ-clone/core-data-object/AdditionalData';
+import City from '@civ-clone/core-city/City';
 import Player from '@civ-clone/core-player/Player';
 import Tile from '@civ-clone/core-world/Tile';
 
 export const getAdditionalData = (
   unitRegistry: UnitRegistry = unitRegistryInstance
 ) => [
+  new AdditionalData(City, 'units', (city: City) =>
+    unitRegistry.getByCity(city)
+  ),
   new AdditionalData(Player, 'units', (player: Player) =>
     unitRegistry.getByPlayer(player)
   ),
