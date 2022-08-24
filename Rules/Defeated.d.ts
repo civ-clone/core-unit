1b4be734d1c8eddbacdc8134edaf68ea4786b425
dom111 <dom111@users.noreply.github.com>
2022-08-24 21:21:59 +0100

    Include the `Action` in `Defeated` `Rule`s so when a `Unit` in a `City` is `Destroyed` it's easy enough to work out if it was a defender.

--- Rules/Defeated.d.ts
+++ Rules/Defeated.d.ts
@@ -1,7 +1,9 @@
 import { IRuleRegistry } from '@civ-clone/core-rule/RuleRegistry';
+import Action from '../Action';
 import Rule from '@civ-clone/core-rule/Rule';
 import Unit from '../Unit';
-export declare class Defeated extends Rule<[Unit, Unit], void> {}
+declare type defeatedArgs = [Unit, Unit, Action];
+export declare class Defeated extends Rule<defeatedArgs, void> {}
 export default Defeated;
 export interface IDefeatedRegistry
-  extends IRuleRegistry<Defeated, [Unit, Unit], void> {}
+  extends IRuleRegistry<Defeated, defeatedArgs, void> {}
